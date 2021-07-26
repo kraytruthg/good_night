96c489193456d502889e48ca35a17fda3c886ee3
Jerry <kraytruthg@gmail.com>
2021-07-26 11:31:52 +0800

    Add User#sleeping method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  has_many :sleeps
+  has_many :sleeps, dependent: :destroy
 
   validates :name, presence: true
+
+  def sleeping?
+    !!sleeps.last && sleeps.last.in_progress?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe "User" do
   subject(:user) { FactoryBot.create(:user) }
 
   it { is_expected.to validate_presence_of(:name) }
+
+  describe "#sleeping?" do
+    context "when there is no sleep records" do
+      it "returns false" do
+        expect(user.sleeping?).to eq(false)
+      end
+    end
+
+    context "when last sleep is finish" do
+      it "returns false" do
+        FactoryBot.create(:sleep, user: user)
+
+        expect(user.sleeping?).to eq(false)
+      end
+    end
+
+    context "when last sleep is in progress" do
+      it "returns true" do
+        FactoryBot.create(:sleep, :in_progress, user: user)
+
+        expect(user.sleeping?).to eq(true)
+      end
+    end
+  end
 end
